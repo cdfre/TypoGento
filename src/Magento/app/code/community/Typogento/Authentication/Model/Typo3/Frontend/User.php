2c2ddf94bb54c3c46dfd57d864eafa6ed3408d13
Artus Kolanowski <artus@ionoi.net>
2013-02-15 14:40:35 +0100

    [bugfix] TYPO3 frontend user authentication in Magento extension
    Processes the generated TYPO3 login directly and forces default
    password transmission strategy.

--- src/Magento/app/code/community/Typogento/Authentication/Model/Typo3/Frontend/User.php
+++ src/Magento/app/code/community/Typogento/Authentication/Model/Typo3/Frontend/User.php
@@ -1,59 +1,54 @@
 <?php
 
 /**
- * TypoGento frontend user model
- *
- * Provides direct access to the TYPO3 fe_users through Magento.
+ * Extends the TYPO3 frontend user model.
  * 
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License, version 2
  */
 class Typogento_Authentication_Model_Typo3_Frontend_User extends Typogento_Replication_Model_Typo3_Frontend_User {
 	
 	/**
-	 * Authenticate frontend user
+	 * Provides TYPO3 frontend user authentication in the box.
 	 * 
-	 * Provides TYPO3 fe_users authentication in the box.
-	 * 
-	 * @param unknown_type $customer
-	 * @param unknown_type $password
+	 * @param string $password
+	 * @return boolean
+	 * @todo Log exceptions
 	 */
 	public function authenticate($password) {
-		//
+		// try authentication
 		try {
 			// if frontend user disabled, not found or detached
-			if (!$this->getId() || !$this->isEnabled()
+			if (!$this->getId() 
+				|| !$this->isEnabled()
 				|| !Mage::helper('typogento_core/typo3')->isFrontendActive()) {
 				return false;
 			}
-			// 
+			// result
 			$authenticated = false;
-			$exclude = array('tx_typogento_sv2');
-			// 
-			$lib = $GLOBALS['TSFE']->fe_user;
-			$lib->formfield_status = 'login';
-			$lib->formfield_uname = 'tx_typogento_login_name';
-			$lib->formfield_uident = 'tx_typogento_login_password';
-			$lib->formfield_chalvalue = 'tx_typogento_login_challenge';
-			$lib->getMethodEnabled = true;
-			t3lib_div::_GETset($this->getData('username'), $lib->formfield_uname);
-			t3lib_div::_GETset($password, $lib->formfield_uident);
-			t3lib_div::_GETset('12345', $lib->formfield_chalvalue);
-			//
-			$info = $lib->getAuthInfoArray();
-			$login = $lib->getLoginFormData();
+			// excluded services
+			$exclude = array('tx_typogento_sv1', 'tx_typogento_sv2');
+			// retrive authentication
+			$authentication = $GLOBALS['TSFE']->fe_user;
+			// create login
+			$login = array(
+				'status' => 'login',
+				'uname' => $this->getData('username'),
+				'uident' => $password,
+				'chalvalue' => ''
+			);
+			// process login
+			$login = $authentication->processLoginData($login, 'normal');
+			// retrive user data
 			$record = $this->getData();
-			// 
-			while (is_object($service = t3lib_div::makeInstanceService('auth', 'authUserFE', $exclude))) {
-				// exclude service
+			// iterate services
+			while (is_object($service = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstanceService('auth', 'authUserFE', $exclude))) {
+				// exclude processed
 				$exclude[] = $service->getServiceKey();
-				//
-				if ($service->getServiceKey() == 'tx_typogento_sv1') {
-					continue;
-				}
-				// initialize service
-				$service->initAuth('authUserFE', $login, $info, $record);
-				// authenticate
+				// initialize current
+				$service->initAuth('authUserFE', $login, $info, $authentication);
+				// perform authentication
 				$result = $service->authUser($record);
+				// cast result
 				$result = intval($result);
 				// validate result
 				if ($result >= 200) {
@@ -67,13 +62,14 @@ class Typogento_Authentication_Model_Typo3_Frontend_User extends Typogento_Repli
 					$authenticated = false;
 					break;
 				}
+				// unset current
 				unset($service);
 			}
-			unset($service);
 		} catch (Exception $e) {
-			return false;
+			// something went wrong
+			$authenticated = false;
 		}
-		
+		// return result
 		return $authenticated;
 	}
 
