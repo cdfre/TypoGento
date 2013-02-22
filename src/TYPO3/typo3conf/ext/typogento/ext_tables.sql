dcee1e14b77c26850179d6571e54fcf17b502563
Artus Kolanowski <artus@ionoi.net>
2013-02-22 02:36:04 +0100

    [task] Remove unnecessary SQL script for caching framework
    The SQL script for the caching tables is no longer necessary since
    they created automatically.

--- src/TYPO3/typo3conf/ext/typogento/ext_tables.sql
+++ src/TYPO3/typo3conf/ext/typogento/ext_tables.sql
@@ -43,29 +43,3 @@ CREATE TABLE tx_typogento_replication_links (
 	UNIQUE KEY target_id (target,provider),
 	UNIQUE KEY source_id (source,provider)
 ) ENGINE=InnoDB;
-
-#
-# Table structure for table 'tx_typogento_cache'.
-#
-CREATE TABLE tx_typogento_cache (
-	id int(11) unsigned NOT NULL auto_increment,
-	identifier varchar(250) DEFAULT '' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	content mediumblob,
-	lifetime int(11) unsigned DEFAULT '0' NOT NULL,
-	PRIMARY KEY (id),
-	KEY cache_id (identifier)
-) ENGINE=InnoDB;
- 
-#
-# Table structure for table 'tx_typogento_cache_tags'.
-#
-CREATE TABLE tx_typogento_cache_tags (
-	id int(11) unsigned NOT NULL auto_increment,
-	identifier varchar(250) DEFAULT '' NOT NULL,
-	tag varchar(250) DEFAULT '' NOT NULL,
-	PRIMARY KEY (id),
-	KEY cache_id (identifier),
-	KEY cache_tag (tag)
-) ENGINE=InnoDB;
-
